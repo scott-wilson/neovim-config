0da26377d160237a4ee298412bb730b28100206e
Scott Wilson <scott@propersquid.com>
2025-08-25 09:15:01 -0700

    Adding config for format on save

--- lua/propersquid/core/profiles.lua
+++ lua/propersquid/core/profiles.lua
@@ -2,6 +2,10 @@ if vim.g.project_profiles ~= nil then
   vim.g.project_profiles = {}
 end
 
+if vim.g.format_on_save ~= nil then
+  vim.g.format = false
+end
+
 local function has_value(arr, val)
   for index, value in ipairs(arr) do
     if value == val then
@@ -26,11 +30,28 @@ local function load_rust_profile()
   end, { silent = true, buffer = bufnr })
 end
 
+local function format_on_save()
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+      local mode = vim.api.nvim_get_mode().mode
+      local filetype = vim.bo.filetype
+
+      if vim.bo.modified == true and mode == 'n' then
+        vim.cmd('lua vim.lsp.buf.format()')
+      end
+    end
+  })
+end
+
 function load_profile()
   if has_value(vim.g.project_profiles, "python") then
     load_python_profile()
   end
   if has_value(vim.g.project_profiles, "rust") then
     load_rust_profile()
+  end
+
+  if vim.g.format_on_save == true then
+    format_on_save()
   end
 end
