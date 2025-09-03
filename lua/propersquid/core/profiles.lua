ea6e86fd9c7a918f67fa1f5ec5c1620c0d30bfe8
Scott Wilson <scott@propersquid.com>
2025-09-02 20:51:18 -0700

    Add neotest python and update profiles

--- lua/propersquid/core/profiles.lua
+++ lua/propersquid/core/profiles.lua
@@ -21,24 +21,35 @@ local function has_value(arr, val)
 end
 
 local function load_python_profile()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
+  require("mason").setup()
+  local mason_lspconfig = require("mason-lspconfig")
+  mason_lspconfig.setup { ensure_installed = { "pyright", "ruff" } }
+  require("lspconfig").pyright.setup { capabilities = capabilities }
+
+  require("neotest").setup({ adapters = { require "neotest-python" } })
 end
 
 local function load_rust_profile()
-  vim.keymap.set("n", "<leader>a", function()
-    -- Supports rust-analyzer's grouping or vim.lsp.buf.codeAction() if you don't want grouping.
-    vim.cmd.RustLsp("codeAction")
-  end, { silent = true, buffer = bufnr })
-  vim.keymap.set("n", "K", function()
-    vim.cmd.RustLsp({ "hover", "actions" })
-  end, { silent = true, buffer = bufnr })
+  require("mason").setup()
+  local mason_lspconfig = require("mason-lspconfig")
+  mason_lspconfig.setup { ensure_installed = { "codelldb" } }
+
+  -- vim.keymap.set("n", "<leader>a", function()
+  --   -- Supports rust-analyzer's grouping or vim.lsp.buf.codeAction() if you don't want grouping.
+  --   vim.cmd.RustLsp("codeAction")
+  -- end, { silent = true, buffer = bufnr })
+  -- vim.keymap.set("n", "K", function()
+  --   vim.cmd.RustLsp({ "hover", "actions" })
+  -- end, { silent = true, buffer = bufnr })
 end
 
 local function format_on_save()
   vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function()
       local mode = vim.api.nvim_get_mode().mode
-      local filetype = vim.bo.filetype
 
       if vim.bo.modified == true and mode == 'n' then
         vim.cmd('lua vim.lsp.buf.format()')
@@ -48,6 +59,10 @@ local function format_on_save()
 end
 
 function load_profile()
+  require("mason").setup()
+  local mason_lspconfig = require("mason-lspconfig")
+  mason_lspconfig.setup { ensure_installed = { "lua_ls" } }
+
   if has_value(vim.g.project_profiles, "python") then
     load_python_profile()
   end
