6a20cea7721349e21f5cd05368cbade4d699f036
Scott Wilson <scott@propersquid.com>
2026-08-18 19:22:29 -0700

    Add quint support

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Quint support
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "quint",
+  callback = function(ev)
+    vim.lsp.start({ name = "quint", cmd = { "quint-language-server", "--stdio" }, root_dir = vim.fs.dirname() })
+  end,
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.qnt",
+  callback = function(ev)
+    vim.bo.filetype = "quint"
+  end,
+})
+vim.filetype.add({ pattern = {
+  ["*.qnt"] = "quint",
+} })
